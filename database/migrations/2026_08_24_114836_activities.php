43f1ed0248ecf30885e8ad096eacdc32757f230b
frank :3 <505537@student.firda.nl>
2026-08-24 14:44:26 +0200

    added activites migration

---
+++ database/migrations/2026_08_24_114836_activities.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->string('uid');
+            $table->dateTime('dt_stamp');
+            $table->dateTime('dt_strat');
+            $table->dateTime('dt_end');
+            $table->tinyText('summary');
+            $table->string('description');
+            $table->string('status');
+            $table->string('text');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};

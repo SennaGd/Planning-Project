be07039072c70bb24ac495c95717495be588409d
SennaGd <sennagrasmeijer123@gmail.com>
2026-09-04 11:32:58 +0200

    Created parse_ics_time inside Controllers/ActivityController. This parses the .now() time format to ICS format

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -58,6 +58,24 @@ class ActivityController extends Controller
 
         return view('ics', compact("activity"));
     }
+    /**
+     * Parsed from format .now()  to ICS format
+     * YYYY-MM-DD HH-MM-SS -> YYYYMMDDTHHMMSSZ
+     * 2026-09-04 11:09:48 -> 20260904T110948Z
+    */
+    public function parse_ics_time(string $str_time)
+    {
+        $year = substr($str_time, 0, 4);
+        $month = substr($str_time, 5, 2);
+        $day = substr($str_time, 8, 2);
+        $hour = substr($str_time, 11, 2);
+        $minute = substr($str_time, 14, 2);
+        $second = substr($str_time, 17, 2);
+
+        $ics_time = $year.$month.$day."T".$hour.$minute.$second."Z";
+
+        return $ics_time;
+    }
     public function generate_ics_feed(): Response
     {
 
@@ -83,23 +101,22 @@ class ActivityController extends Controller
         foreach ($id_list_TEST as $id){
             $activity = Activity::where("prod_id", $id)->firstOrFail();
             if ($activity) {
-                $parsed_dt_start = $activity
-                    ->dt_start
-                    ->utc()
-                    ->format('Ymd\THis\Z');
+                $parsed_dt_start = ActivityController::parse_ics_time(
+                    $activity->dt_start
+                );
 
-                $parsed_dt_end = $activity
-                    ->dt_end
-                    ->utc()
-                    ->format('Ymd\THis\Z');
+                $parsed_dt_end = ActivityController::parse_ics_time(
+                    $activity->dt_end
+                );
+
 
 
                 $event = implode("\r\n", [
                     "BEGIN:VEVENT",
                     "UID:".uniqid()."@firda-planning.com",
                     "DTSTAMP:".now()->utc()->format('Ymd\THis\Z'),
-                    "DTSTART:$activity->dt_start",
-                    "DTEND:$activity->dt_end",
+                    "DTSTART:$parsed_dt_start",
+                    "DTEND:$parsed_dt_end",
                     "SUMMARY:$activity->summary",
                     "LOCATION:$activity->location",
                     "END:VEVENT",

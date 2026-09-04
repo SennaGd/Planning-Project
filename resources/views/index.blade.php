ee9e19c42e8562e45b10f14b472ad34eae4f6543
Tiemen Boersma <97064313+Haunted12@users.noreply.github.com>
2026-09-04 09:11:47 +0200

    #22 Refine planning table spacing and headers
    Updated the index and lesplein planning tables to improve layout consistency: removed extra table margin, added header/body cell padding, and moved the light background styling to table headers. Also renamed the final column label from "Status" to "Opmerkingen" and aligned top bar background behavior in the shared layout and index filter bar.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,5 @@
 <x-SchipholLayout>
-    <div class="flex w-full items-center bg-gray-300 px-6 py-3 dark:bg-schiphol-dark_gray transition-colors duration-200" id="filterbar">
+    <div class="flex w-full items-center px-6 py-3 dark:bg-schiphol-dark_gray transition-colors duration-200" id="filterbar">
         <form method="GET" action="/" class="flex items-center gap-3">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 shrink-0">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
@@ -9,29 +9,28 @@
         <div class="flex-1"></div>
         <a href="">Kalenderabonomenten</a>
     </div>
-    <div id="planningtable">
-        <table class="table-auto text-left w-full m-3">
-            <thead class="dark:bg-schiphol-dark_gray transition-colors duration-200">
-            <tr class="dark:text-schiphol-light_gray transition-colors duration-200 text-2xl border-b border-gray-300 dark:border-schiphol-dark_gray">
-                <th scope="col">Tijd</th>
-                <th scope="col">Datum</th>
-                <th scope="col">Bestemming</th>
-                <th scope="col">Gate</th>
-                <th scope="col">Vluchtnummer</th>
-                <th scope="col">Vliegmaatschapij</th>
-                <th scope="col">Status</th>
+        <table class="table-auto text-left w-full">
+            <thead class="dark:bg-schiphol-dark_gray bg-gray-300 transition-colors duration-200">
+            <tr class="dark:text-schiphol-light_gray transition-colors duration-200 text-2xl dark:border-schiphol-dark_gray">
+                <th scope="col" class="px-4 py-2">Tijd</th>
+                <th scope="col" class="px-4 py-2">Datum</th>
+                <th scope="col" class="px-4 py-2">Bestemming</th>
+                <th scope="col" class="px-4 py-2">Gate</th>
+                <th scope="col" class="px-4 py-2">Vluchtnummer</th>
+                <th scope="col" class="px-4 py-2">Vliegmaatschapij</th>
+                <th scope="col" class="px-4 py-2">Opmerkingen</th>
             </tr>
             </thead>
             <tbody>
                 @foreach($activities as $activity)
                     @if(\Carbon\Carbon::parse($activity->dt_strat)->isToday())
                         <tr class="dark:border-schiphol-dark_gray transition-colors duration-200 text-1xl">
-                            <td>{{ \Carbon\Carbon::parse($activity->dt_strat)->format('H:i') }} - {{ \Carbon\Carbon::parse($activity->dt_end)->format('H:i') }}</td>
-                            <td>{{ \Carbon\Carbon::parse($activity->dt_strat)->format('d-m-Y') }}</td>
-                            <td>{{ $activity->summary }}</td>
-                            <td>{{ $activity->location }}</td>
-                            <td>{{ $activity->class }}</td>
-                            <td>{{ $activity->attendee}}</td>
+                            <td class="px-4 py-2">{{ \Carbon\Carbon::parse($activity->dt_strat)->format('H:i') }} - {{ \Carbon\Carbon::parse($activity->dt_end)->format('H:i') }}</td>
+                            <td class="px-4 py-2">{{ \Carbon\Carbon::parse($activity->dt_strat)->format('d-m-Y') }}</td>
+                            <td class="px-4 py-2">{{ $activity->summary }}</td>
+                            <td class="px-4 py-2">{{ $activity->location }}</td>
+                            <td class="px-4 py-2">{{ $activity->class }}</td>
+                            <td class="px-4 py-2">{{ $activity->attendee}}</td>
                         </tr>
                     @endif
                 @endforeach
--- resources/views/lesplein.blade.php
+++ resources/views/lesplein.blade.php
@@ -1,27 +1,27 @@
 <x-SchipholLayout>
     <div id="planningtable">
-        <table class="table-auto text-left w-full m-3">
-            <thead class="dark:bg-schiphol-dark_gray transition-colors duration-200">
-            <tr class="dark:text-schiphol-light_gray transition-colors duration-200 text-2xl border-b border-gray-300 dark:border-schiphol-dark_gray">
-                <th scope="col">Tijd</th>
-                <th scope="col">Datum</th>
-                <th scope="col">Bestemming</th>
-                <th scope="col">Gate</th>
-                <th scope="col">Vluchtnummer</th>
-                <th scope="col">Vliegmaatschapij</th>
-                <th scope="col">Status</th>
+        <table class="table-auto text-left w-full">
+            <thead class="dark:bg-schiphol-dark_gray bg-gray-300 transition-colors duration-200">
+            <tr class="dark:text-schiphol-light_gray transition-colors duration-200 text-2xl dark:border-schiphol-dark_gray">
+                <th scope="col" class="px-4 py-2">Tijd</th>
+                <th scope="col" class="px-4 py-2">Datum</th>
+                <th scope="col" class="px-4 py-2">Bestemming</th>
+                <th scope="col" class="px-4 py-2">Gate</th>
+                <th scope="col" class="px-4 py-2">Vluchtnummer</th>
+                <th scope="col" class="px-4 py-2">Vliegmaatschapij</th>
+                <th scope="col" class="px-4 py-2">Opmerkingen</th>
             </tr>
             </thead>
             <tbody>
                 @foreach($activities as $activity)
                     @if(\Carbon\Carbon::parse($activity->dt_strat)->isToday())
                         <tr class="dark:border-schiphol-dark_gray transition-colors duration-200 text-1xl">
-                            <td>{{ \Carbon\Carbon::parse($activity->dt_strat)->format('H:i') }} - {{ \Carbon\Carbon::parse($activity->dt_end)->format('H:i') }}</td>
-                            <td>{{ \Carbon\Carbon::parse($activity->dt_strat)->format('d-m-Y') }}</td>
-                            <td>{{ $activity->summary }}</td>
-                            <td>{{ $activity->location }}</td>
-                            <td>{{ $activity->class }}</td>
-                            <td>{{ $activity->attendee}}</td>
+                            <td class="px-4 py-2">{{ \Carbon\Carbon::parse($activity->dt_strat)->format('H:i') }} - {{ \Carbon\Carbon::parse($activity->dt_end)->format('H:i') }}</td>
+                            <td class="px-4 py-2">{{ \Carbon\Carbon::parse($activity->dt_strat)->format('d-m-Y') }}</td>
+                            <td class="px-4 py-2">{{ $activity->summary }}</td>
+                            <td class="px-4 py-2">{{ $activity->location }}</td>
+                            <td class="px-4 py-2">{{ $activity->class }}</td>
+                            <td class="px-4 py-2">{{ $activity->attendee}}</td>
                         </tr>
                     @endif
                 @endforeach

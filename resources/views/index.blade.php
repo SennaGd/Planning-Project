ab16908b74c6b6c5202a525f1ff76723379566e4
Tiemen Boersma <97064313+Haunted12@users.noreply.github.com>
2026-09-04 08:08:04 +0200

    #22 Display today's activities on home page
    Route the home page through the activity controller and render a styled table of today's upcoming activities. The view now includes a status column, filters activities to only those occurring today, and applies improved table styling for the planning board.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,27 +10,30 @@
         <a href="">Kalenderabonomenten</a>
     </div>
     <div id="planningtable">
-        <table>
-            <thead>
-            <tr>
+        <table class="table-auto text-left w-full m-3">
+            <thead class="dark:bg-schiphol-dark_gray transition-colors duration-200">
+            <tr class="dark:text-schiphol-light_gray transition-colors duration-200 text-2xl border-b border-gray-300 dark:border-schiphol-dark_gray">
                 <th scope="col">Tijd</th>
                 <th scope="col">Datum</th>
                 <th scope="col">Bestemming</th>
                 <th scope="col">Gate</th>
                 <th scope="col">Vluchtnummer</th>
                 <th scope="col">Vliegmaatschapij</th>
+                <th scope="col">Status</th>
             </tr>
             </thead>
             <tbody>
                 @foreach($activities as $activity)
-                    <tr>
-                        <td>{{ \Carbon\Carbon::parse($activity->dt_strat)->format('H:i') }} - {{ \Carbon\Carbon::parse($activity->dt_end)->format('H:i') }}</td>
-                        <td>{{ \Carbon\Carbon::parse($activity->dt_strat)->format('d-m-Y') }}</td>
-                        <td>{{ $activity->summary }}</td>
-                        <td>{{ $activity->location }}</td>
-                        <td>{{ $activity->class }}</td>
-                        <td>{{ $activity->attendee}}</td>
-                    </tr>
+                    @if(\Carbon\Carbon::parse($activity->dt_strat)->isToday())
+                        <tr class="dark:border-schiphol-dark_gray transition-colors duration-200 text-1xl">
+                            <td>{{ \Carbon\Carbon::parse($activity->dt_strat)->format('H:i') }} - {{ \Carbon\Carbon::parse($activity->dt_end)->format('H:i') }}</td>
+                            <td>{{ \Carbon\Carbon::parse($activity->dt_strat)->format('d-m-Y') }}</td>
+                            <td>{{ $activity->summary }}</td>
+                            <td>{{ $activity->location }}</td>
+                            <td>{{ $activity->class }}</td>
+                            <td>{{ $activity->attendee}}</td>
+                        </tr>
+                    @endif
                 @endforeach
             </tbody>
         </table>
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ActivityController;
 
 
-Route::view('/', 'index')->name('home');
+Route::get('/', [ActivityController::class, 'index'])->name('home');
 Route::view('/lesplein', 'lesplein')->name('lesplein');
 
 Route::get('/ics/{activity}', [ActivityController::class, 'show']);
